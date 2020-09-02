2bb6c0333cb0c2f8ee9700797cb0d625af73fbaa
Pavel Khlebko <pavel.khlebko@clarivate.com>
2020-09-02 17:09:23 +0300

    Added http server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { Line } from './classes/line.js';
 import { Device } from './classes/device.js';
 import { appConfig } from './configs/config.js';
+import { TcpServer } from './classes/tcp-server.js';
 
 const driver = {
   lines: [],
@@ -11,6 +12,12 @@ driver.lines = appConfig.lines.map(item => new Line(item));
 
 driver.devices = appConfig.devices.map(item => new Device(item, driver.lines));
 
-Promise.all(driver.devices[0].read(['ia01', 'ia03'])).then(values => {
+/* Promise.all(driver.devices[0].read(['ia01', 'ia03'])).then(values => {
   console.log(values);
 });
+ */
+
+const tcpServer = new TcpServer();
+
+tcpServer.start(2222);
+setTimeout(() => tcpServer.stop(), 5000000);
